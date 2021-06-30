aa1e2c8f59d899665c1c42a47ae37638020029e9
ignaciocervino <ignaciocervino@gmail.com>
2021-06-30 01:39:49 -0300

    #07 Read criptocurrencie

--- js/app.js
+++ js/app.js
@@ -1,4 +1,11 @@
 const criptomonedasSelect = document.querySelector('#criptomonedas');
+const monedaSelect = document.querySelector('#moneda');
+const formulario = document.querySelector('#formulario');
+
+const objBusqueda={
+    moneda: '',
+    criptomoneda: ''
+}
 
 //Crear un Promise
 const obtenerCriptomonedas = criptomonedas => new Promise(resolve => {
@@ -7,6 +14,11 @@ const obtenerCriptomonedas = criptomonedas => new Promise(resolve => {
 
 document.addEventListener('DOMContentLoaded',()=>{
     consultadCriptomonedas();
+    formulario.addEventListener('submit',submitFormulario);
+
+    criptomonedasSelect.addEventListener('change',leerValor);
+    monedaSelect.addEventListener('change',leerValor);
+
 });
 
 function consultadCriptomonedas(){
@@ -27,4 +39,26 @@ function selectCriptomonedas(criptomonedas){
 
         criptomonedasSelect.appendChild(option);
     });
+}
+
+function leerValor(e){
+    objBusqueda[e.target.name] = e.target.value;
+}
+
+function submitFormulario(e){
+    e.preventDefault();
+
+    //Validar
+    const {moneda,criptomoneda} = objBusqueda;
+
+    if (moneda===''||criptomoneda==='') {
+        mostrarAlerta('Ambos campos son obligatorios');
+        return;
+    }
+
+
+}
+
+function mostrarAlerta(){
+    
 }
